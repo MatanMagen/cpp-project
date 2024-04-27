d4ca97ab7b79431926ef69a7acb2b2cb6da608f5
MatanMagen <matanmagen2002@gmail.com>
2024-04-27 14:05:08 +0300

    add walls logic

--- point.cpp
+++ point.cpp
@@ -63,13 +63,13 @@ void Point::move(GameConfig::eKeys key)
 	x += diff_x;
 	if (x > GameConfig::GAME_WIDTH)
 		x = 1;
-	else if (x == 0)
+	else if (x == -1)
 		x = GameConfig::GAME_WIDTH;
 
 	y += diff_y;
-	if (y > GameConfig::GAME_HEIGHT)
+	if (y > GameConfig::GAME_HEIGHT + 3)
 		y = 1;
-	else if (y == 0)
-		y = GameConfig::GAME_HEIGHT;
+	else if (y == -1)
+		y = GameConfig::GAME_HEIGHT + 2;
 	
 }
--- ship.cpp
+++ ship.cpp
@@ -9,37 +9,51 @@
 
 void Ship::move(GameConfig::eKeys direction, char ch, Board& board)
 {
-
+	bool hitwall = false;
 	if (direction != GameConfig::eKeys::PAUSE)
 	{
-
+		//check if not wall infront
+		Point temp[4];
 		for (int i = 0; i < size; i++)
 		{
-			pos[i].draw(' ', GameConfig::COLORS[0]);
+			temp[i] = pos[i];
+			temp[i].move(direction);
 		}
 
 		for (int i = 0; i < size; i++)
 		{
-			board.getBoard()[pos[i].getY()][pos[i].getX() ] = ' ';
+			if (board.getBoard()[temp[i].getY()][temp[i].getX()] == 'W')
+				hitwall = true;
 		}
 
-
-		for (int i = 0; i < size; i++)
+		//actuall move
+		if (!hitwall)
 		{
-			pos[i].move(direction);
-			pos[i].draw(ch, GameConfig::COLORS[0]);
+			for (int i = 0; i < size; i++)
+			{
+				pos[i].draw(' ', GameConfig::COLORS[0]);
+			}
+
+			for (int i = 0; i < size; i++)
+			{
+				board.getBoard()[pos[i].getY()][pos[i].getX()] = ' ';
+			}
+
+
+			for (int i = 0; i < size; i++)
+			{
+				pos[i].move(direction);
+				pos[i].draw(ch, GameConfig::COLORS[0]);
+			}
+
+			for (int i = 0; i < size; i++)
+			{
+				board.getBoard()[pos[i].getY()][pos[i].getX()] = ch;
+			}
 		}
 
-		for (int i = 0; i < size; i++)
-		{
-			board.getBoard()[pos[i].getY()][pos[i].getX()] = ch;
-		}
 
 		
-	}
-	else
-	{
-
 	}
 
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), GameConfig::COLORS[0]);
